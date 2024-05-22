6461ba1ea4dce9be3c26fbe2d6ad7d04aa06b2d8
AlbertTM8 <115167346+AlbertTM8@users.noreply.github.com>
2024-05-22 11:00:24 +0100

    Update gitprofile.config.ts

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -76,14 +76,14 @@ const CONFIG = {
         description: 'Masters Thesis. Generating crack patterns procedurally through hybrid physical and heurisitic approach (Artistic and Mathematical). Based on "Generating Surface Crack Patterns" from UC Berkely.',
         imageUrl:
           'https://github.com/AlbertTM8/AlbertTM8.github.io/blob/main/Screenshot%202024-05-21%20144213.png?raw=true',
-        link: 'https://github.com/AlbertTM8/Animation-Software-Engineering-Project',
+        link: 'https://github.com/AlbertTM8/Crack-Simulation-Hybrid',
       },
       {
         title: 'The Hunted - UE5 Game',
         description: 'WIP Progress game in Unreal Engine. Procedurally generated infinite maze map and listen server setup.',
         imageUrl:
           'https://github.com/AlbertTM8/AlbertTM8.github.io/blob/main/Screenshot%202024-05-21%20144320.png?raw=true',
-        link: 'https://github.com/AlbertTM8/Animation-Software-Engineering-Project',
+        link: 'https://github.com/AlbertTM8/HuntedGameUnreal',
       },
     ],
   },
