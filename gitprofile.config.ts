d533ab6ddba793711bcf0b7e6402f09d2acb3d1b
AlbertTM8 <115167346+AlbertTM8@users.noreply.github.com>
2024-12-07 17:04:51 +0000

    Update gitprofile.config.ts

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -43,6 +43,14 @@ const CONFIG = {
     // To hide the `External Projects` section, keep it empty.
     projects: [
       {
+        title: 'Human Procedural Animations',
+        description:
+          'Using Unreal Engine Control Rig to create flexible and convenient procedural animation for humans',
+        imageUrl:
+          'https://github.com/AlbertTM8/AlbertTM8.github.io/blob/main/widen_1220x0.png?raw=true',
+        link: 'https://github.com/AlbertTM8/ProceduralAnimDemos',
+      },
+      {
         title: 'Stable Diffusion Texture Map Generator',
         description:
           'Unreal plug-in for creating texture maps through AI diffusion models (Stable Diffusion). Built for artists using PySide.',
